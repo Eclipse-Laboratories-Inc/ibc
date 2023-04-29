9c05c27957f5e0ac40719f8cd0936eae7510bc3b
Kevin Ji <1146876+kevinji@users.noreply.github.com>
2023-04-28 20:15:31 -0700

    scripts: Add channel commands

--- scripts/set-up-ibc-connection.sh
+++ scripts/set-up-ibc-connection.sh
@@ -8,6 +8,9 @@ CHAIN_A=ibc-a
 CHAIN_B=ibc-b
 CLIENT_ID=xx-eclipse-0
 CONNECTION_ID=connection-0
+PORT_ID=test-msgs
+CHANNEL_ID=channel-0
+IBC_ACCOUNT=A7NJxtiKpEFL4TSTygkKSkf5b2g719DJbvQPRr4moUHD
 export RUST_LOG=info
 
 : <<'END_COMMENT'
@@ -45,3 +48,39 @@ END_COMMENT
   | "$CLI" tx --endpoint "$ENDPOINT_B" connection open-confirm
 "$CLI" generate --endpoint "$ENDPOINT_B" client update "$CLIENT_ID" \
   | "$CLI" tx --endpoint "$ENDPOINT_A" client update
+
+solana airdrop 100 "$IBC_ACCOUNT" --url "$ENDPOINT_A"
+solana airdrop 100 "$IBC_ACCOUNT" --url "$ENDPOINT_B"
+
+"$CLI" tx --endpoint "$ENDPOINT_A" port bind "$PORT_ID"
+"$CLI" tx --endpoint "$ENDPOINT_B" port bind "$PORT_ID"
+
+"$CLI" generate --endpoint "$ENDPOINT_A" client update "$CLIENT_ID" \
+  | "$CLI" tx --endpoint "$ENDPOINT_B" client update
+
+"$CLI" generate --endpoint "$ENDPOINT_B" client update "$CLIENT_ID" \
+  | "$CLI" tx --endpoint "$ENDPOINT_A" client update
+
+"$CLI" generate --endpoint "$ENDPOINT_B" --cpty-endpoint "$ENDPOINT_A" channel open-init "$CONNECTION_ID" "$PORT_ID" "$PORT_ID" \
+  | "$CLI" tx --endpoint "$ENDPOINT_A" channel open-init
+"$CLI" generate --endpoint "$ENDPOINT_A" client update "$CLIENT_ID" \
+  | "$CLI" tx --endpoint "$ENDPOINT_B" client update
+
+"$CLI" generate --endpoint "$ENDPOINT_A" --cpty-endpoint "$ENDPOINT_B" channel open-try "$CLIENT_ID" "$CONNECTION_ID" "$PORT_ID" "$PORT_ID" "$CHANNEL_ID" \
+  | "$CLI" tx --endpoint "$ENDPOINT_B" channel open-try
+"$CLI" generate --endpoint "$ENDPOINT_B" client update "$CLIENT_ID" \
+  | "$CLI" tx --endpoint "$ENDPOINT_A" client update
+
+"$CLI" generate --endpoint "$ENDPOINT_B" --cpty-endpoint "$ENDPOINT_A" channel open-ack "$CLIENT_ID" "$PORT_ID" "$CHANNEL_ID" "$PORT_ID" "$CHANNEL_ID" \
+  | "$CLI" tx --endpoint "$ENDPOINT_A" channel open-ack
+
+"$CLI" generate --endpoint "$ENDPOINT_A" client update "$CLIENT_ID" \
+  | "$CLI" tx --endpoint "$ENDPOINT_B" client update
+
+"$CLI" generate --endpoint "$ENDPOINT_A" --cpty-endpoint "$ENDPOINT_B" channel open-confirm "$CLIENT_ID" "$PORT_ID" "$CHANNEL_ID" "$PORT_ID" "$CHANNEL_ID" \
+  | "$CLI" tx --endpoint "$ENDPOINT_B" channel open-confirm
+"$CLI" generate --endpoint "$ENDPOINT_B" client update "$CLIENT_ID" \
+  | "$CLI" tx --endpoint "$ENDPOINT_A" client update
+
+solana airdrop 100 "$IBC_ACCOUNT" --url "$ENDPOINT_A"
+solana airdrop 100 "$IBC_ACCOUNT" --url "$ENDPOINT_B"
