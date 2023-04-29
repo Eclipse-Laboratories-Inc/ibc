62f35808b000ed2a465cd5a2902eda2a0dcdc408
Kevin Ji <1146876+kevinji@users.noreply.github.com>
2023-04-28 19:45:08 -0700

    cli(generate): Add channel commands

--- cli/src/generate.rs
+++ cli/src/generate.rs
@@ -7,17 +7,27 @@ use {
     eclipse_ibc_state::{internal_path::ConsensusHeightsPath, IbcAccountData, IbcState, IbcStore},
     ibc::core::{
         ics03_connection::version::{get_compatible_versions, Version as ConnectionVersion},
-        ics24_host::path::{ClientConsensusStatePath, ClientStatePath, ConnectionPath},
+        ics24_host::path::{
+            ChannelEndPath, ClientConsensusStatePath, ClientStatePath, ConnectionPath,
+        },
     },
     ibc_proto::{
         ibc::core::{
+            channel::v1::{
+                Channel as RawChannel, Counterparty as RawChannelCounterparty,
+                MsgChannelOpenAck as RawMsgChannelOpenAck,
+                MsgChannelOpenConfirm as RawMsgChannelOpenConfirm,
+                MsgChannelOpenInit as RawMsgChannelOpenInit,
+                MsgChannelOpenTry as RawMsgChannelOpenTry, Order as RawOrder, State as RawState,
+            },
             client::v1::{
                 MsgCreateClient as RawMsgCreateClient, MsgUpdateClient as RawMsgUpdateClient,
                 MsgUpgradeClient as RawMsgUpgradeClient,
             },
             commitment::v1::{MerklePrefix as RawMerklePrefix, MerkleProof as RawMerkleProof},
             connection::v1::{
-                Counterparty as RawCounterparty, MsgConnectionOpenAck as RawMsgConnectionOpenAck,
+                Counterparty as RawConnectionCounterparty,
+                MsgConnectionOpenAck as RawMsgConnectionOpenAck,
                 MsgConnectionOpenConfirm as RawMsgConnectionOpenConfirm,
                 MsgConnectionOpenInit as RawMsgConnectionOpenInit,
                 MsgConnectionOpenTry as RawMsgConnectionOpenTry,
@@ -199,8 +209,8 @@ impl ConnectionMsg {
                 client_id_on_a,
                 client_id_on_b,
             } => {
-                let counterparty = RawCounterparty {
-                    client_id: client_id_on_b.to_owned(),
+                let counterparty = RawConnectionCounterparty {
+                    client_id: client_id_on_b.clone(),
                     connection_id: "".to_owned(),
                     prefix: Some(RawMerklePrefix {
                         key_prefix: eclipse_chain::COMMITMENT_PREFIX.to_vec(),
@@ -208,7 +218,7 @@ impl ConnectionMsg {
                 };
 
                 let msg = RawMsgConnectionOpenInit {
-                    client_id: client_id_on_a.to_owned(),
+                    client_id: client_id_on_a.clone(),
                     counterparty: Some(counterparty),
                     version: Some(ConnectionVersion::default().into()),
                     delay_period: DELAY_PERIOD_NANOS,
@@ -223,9 +233,9 @@ impl ConnectionMsg {
                 client_id_on_a,
                 connection_id_on_a,
             } => {
-                let counterparty = RawCounterparty {
-                    client_id: client_id_on_a.to_owned(),
-                    connection_id: connection_id_on_a.to_owned(),
+                let counterparty = RawConnectionCounterparty {
+                    client_id: client_id_on_a.clone(),
+                    connection_id: connection_id_on_a.clone(),
                     prefix: Some(RawMerklePrefix {
                         key_prefix: eclipse_chain::COMMITMENT_PREFIX.to_vec(),
                     }),
@@ -287,7 +297,7 @@ impl ConnectionMsg {
 
                 #[allow(deprecated)]
                 let msg = RawMsgConnectionOpenTry {
-                    client_id: client_id_on_b.to_owned(),
+                    client_id: client_id_on_b.clone(),
                     previous_connection_id: "".to_owned(),
                     client_state,
                     counterparty: Some(counterparty),
@@ -368,8 +378,8 @@ impl ConnectionMsg {
                 }
 
                 let msg = RawMsgConnectionOpenAck {
-                    connection_id: connection_id_on_a.to_owned(),
-                    counterparty_connection_id: connection_id_on_b.to_owned(),
+                    connection_id: connection_id_on_a.clone(),
+                    counterparty_connection_id: connection_id_on_b.clone(),
                     version: Some(ConnectionVersion::default().into()),
                     client_state,
                     proof_height: Some(consensus_height_of_b_on_a.into()),
@@ -420,7 +430,241 @@ impl ConnectionMsg {
                 }
 
                 let msg = RawMsgConnectionOpenConfirm {
-                    connection_id: connection_id_on_b.to_owned(),
+                    connection_id: connection_id_on_b.clone(),
+                    proof_ack: proof_ack.encode_to_vec(),
+                    proof_height: Some(consensus_height_of_a_on_b.into()),
+                    signer: "".to_owned(),
+                };
+
+                print_json(msg)?;
+                Ok(())
+            }
+        }
+    }
+}
+
+#[allow(clippy::enum_variant_names)]
+#[derive(Clone, Debug, Subcommand)]
+enum ChannelMsg {
+    OpenInit {
+        connection_id_on_a: String,
+        port_id_on_a: String,
+        port_id_on_b: String,
+    },
+    OpenTry {
+        client_id_on_b: String,
+        connection_id_on_b: String,
+        port_id_on_b: String,
+        port_id_on_a: String,
+        channel_id_on_a: String,
+    },
+    OpenAck {
+        client_id_on_a: String,
+        port_id_on_a: String,
+        channel_id_on_a: String,
+        port_id_on_b: String,
+        channel_id_on_b: String,
+    },
+    OpenConfirm {
+        client_id_on_b: String,
+        port_id_on_b: String,
+        channel_id_on_b: String,
+        port_id_on_a: String,
+        channel_id_on_a: String,
+    },
+}
+
+impl ChannelMsg {
+    async fn generate(
+        &self,
+        rpc_client: &RpcClient,
+        cpty_rpc_client: &RpcClient,
+    ) -> anyhow::Result<()> {
+        match self {
+            Self::OpenInit {
+                connection_id_on_a,
+                port_id_on_a,
+                port_id_on_b,
+            } => {
+                let counterparty = RawChannelCounterparty {
+                    port_id: port_id_on_b.clone(),
+                    channel_id: "".to_owned(),
+                };
+
+                let channel = RawChannel {
+                    state: RawState::Init.into(),
+                    ordering: RawOrder::Ordered.into(),
+                    counterparty: Some(counterparty),
+                    connection_hops: vec![connection_id_on_a.clone()],
+                    version: "".to_owned(),
+                };
+
+                let msg = RawMsgChannelOpenInit {
+                    port_id: port_id_on_a.clone(),
+                    channel: Some(channel),
+                    signer: "".to_owned(),
+                };
+
+                print_json(msg)?;
+                Ok(())
+            }
+            Self::OpenTry {
+                client_id_on_b,
+                connection_id_on_b,
+                port_id_on_b,
+                port_id_on_a,
+                channel_id_on_a,
+            } => {
+                let counterparty = RawChannelCounterparty {
+                    port_id: port_id_on_a.clone(),
+                    channel_id: channel_id_on_a.clone(),
+                };
+
+                let channel = RawChannel {
+                    state: RawState::Tryopen.into(),
+                    ordering: RawOrder::Ordered.into(),
+                    counterparty: Some(counterparty),
+                    connection_hops: vec![connection_id_on_b.clone()],
+                    version: "".to_owned(),
+                };
+
+                let ibc_store = get_ibc_store(rpc_client).await?;
+                let ibc_state = get_ibc_state(&ibc_store)?;
+
+                let ibc_latest_version = ibc_store
+                    .read()?
+                    .latest_version()
+                    .ok_or_else(|| anyhow!("No IBC state versions found"))?;
+                let ibc_latest_height = eclipse_chain::height_of_slot(ibc_latest_version)?;
+
+                let proof_init = existence_proof_to_merkle_proof(ibc_state.get_proof(
+                    &ChannelEndPath::new(&port_id_on_a.parse()?, &channel_id_on_a.parse()?),
+                )?);
+
+                let cpty_ibc_store = get_ibc_store(cpty_rpc_client).await?;
+                let cpty_ibc_state = get_ibc_state(&cpty_ibc_store)?;
+
+                let consensus_height_of_a_on_b = *cpty_ibc_state
+                    .get(&ConsensusHeightsPath(client_id_on_b.parse()?))?
+                    .ok_or_else(|| {
+                        anyhow!("Consensus heights not found for client ID {client_id_on_b}")
+                    })?
+                    .heights
+                    .last()
+                    .ok_or_else(|| {
+                        anyhow!("No consensus heights found for client ID {client_id_on_b}")
+                    })?;
+
+                if consensus_height_of_a_on_b < ibc_latest_height {
+                    bail!("Height of chain A on chain B is not recent enough; {consensus_height_of_a_on_b} < {ibc_latest_height}");
+                }
+
+                #[allow(deprecated)]
+                let msg = RawMsgChannelOpenTry {
+                    port_id: port_id_on_b.clone(),
+                    previous_channel_id: "".to_owned(),
+                    channel: Some(channel),
+                    counterparty_version: "".to_owned(),
+                    proof_init: proof_init.encode_to_vec(),
+                    proof_height: Some(consensus_height_of_a_on_b.into()),
+                    signer: "".to_owned(),
+                };
+
+                print_json(msg)?;
+                Ok(())
+            }
+            Self::OpenAck {
+                client_id_on_a,
+                port_id_on_a,
+                channel_id_on_a,
+                port_id_on_b,
+                channel_id_on_b,
+            } => {
+                let ibc_store = get_ibc_store(rpc_client).await?;
+                let ibc_state = get_ibc_state(&ibc_store)?;
+
+                let ibc_latest_version = ibc_store
+                    .read()?
+                    .latest_version()
+                    .ok_or_else(|| anyhow!("No IBC state versions found"))?;
+                let ibc_latest_height = eclipse_chain::height_of_slot(ibc_latest_version)?;
+
+                let proof_try = existence_proof_to_merkle_proof(ibc_state.get_proof(
+                    &ChannelEndPath::new(&port_id_on_b.parse()?, &channel_id_on_b.parse()?),
+                )?);
+
+                let cpty_ibc_store = get_ibc_store(cpty_rpc_client).await?;
+                let cpty_ibc_state = get_ibc_state(&cpty_ibc_store)?;
+
+                let consensus_height_of_b_on_a = *cpty_ibc_state
+                    .get(&ConsensusHeightsPath(client_id_on_a.parse()?))?
+                    .ok_or_else(|| {
+                        anyhow!("Consensus heights not found for client ID {client_id_on_a}")
+                    })?
+                    .heights
+                    .last()
+                    .ok_or_else(|| {
+                        anyhow!("No consensus heights found for client ID {client_id_on_a}")
+                    })?;
+
+                if consensus_height_of_b_on_a < ibc_latest_height {
+                    bail!("Height of chain B on chain A is not recent enough; {consensus_height_of_b_on_a} < {ibc_latest_height}");
+                }
+
+                let msg = RawMsgChannelOpenAck {
+                    port_id: port_id_on_a.clone(),
+                    channel_id: channel_id_on_a.clone(),
+                    counterparty_channel_id: channel_id_on_b.clone(),
+                    counterparty_version: "".to_owned(),
+                    proof_try: proof_try.encode_to_vec(),
+                    proof_height: Some(consensus_height_of_b_on_a.into()),
+                    signer: "".to_owned(),
+                };
+
+                print_json(msg)?;
+                Ok(())
+            }
+            Self::OpenConfirm {
+                client_id_on_b,
+                port_id_on_b,
+                channel_id_on_b,
+                port_id_on_a,
+                channel_id_on_a,
+            } => {
+                let ibc_store = get_ibc_store(rpc_client).await?;
+                let ibc_state = get_ibc_state(&ibc_store)?;
+
+                let ibc_latest_version = ibc_store
+                    .read()?
+                    .latest_version()
+                    .ok_or_else(|| anyhow!("No IBC state versions found"))?;
+                let ibc_latest_height = eclipse_chain::height_of_slot(ibc_latest_version)?;
+
+                let proof_ack = existence_proof_to_merkle_proof(ibc_state.get_proof(
+                    &ChannelEndPath::new(&port_id_on_a.parse()?, &channel_id_on_a.parse()?),
+                )?);
+
+                let cpty_ibc_store = get_ibc_store(cpty_rpc_client).await?;
+                let cpty_ibc_state = get_ibc_state(&cpty_ibc_store)?;
+
+                let consensus_height_of_a_on_b = *cpty_ibc_state
+                    .get(&ConsensusHeightsPath(client_id_on_b.parse()?))?
+                    .ok_or_else(|| {
+                        anyhow!("Consensus heights not found for client ID {client_id_on_b}")
+                    })?
+                    .heights
+                    .last()
+                    .ok_or_else(|| {
+                        anyhow!("No consensus heights found for client ID {client_id_on_b}")
+                    })?;
+
+                if consensus_height_of_a_on_b < ibc_latest_height {
+                    bail!("Height of chain A on chain B is not recent enough; {consensus_height_of_a_on_b} < {ibc_latest_height}");
+                }
+
+                let msg = RawMsgChannelOpenConfirm {
+                    port_id: port_id_on_b.clone(),
+                    channel_id: channel_id_on_b.clone(),
                     proof_ack: proof_ack.encode_to_vec(),
                     proof_height: Some(consensus_height_of_a_on_b.into()),
                     signer: "".to_owned(),
@@ -439,6 +683,8 @@ enum MsgKind {
     Client(ClientMsg),
     #[command(subcommand)]
     Connection(ConnectionMsg),
+    #[command(subcommand)]
+    Channel(ChannelMsg),
 }
 
 #[derive(Debug, Parser)]
@@ -471,6 +717,12 @@ pub(crate) async fn run(
             msg.generate(&rpc_client).await?;
         }
         MsgKind::Connection(msg) => {
+            let cpty_endpoint =
+                cpty_endpoint.ok_or_else(|| anyhow!("Must specify counterparty endpoint"))?;
+            let cpty_rpc_client = RpcClient::new(cpty_endpoint);
+            msg.generate(&rpc_client, &cpty_rpc_client).await?;
+        }
+        MsgKind::Channel(msg) => {
             let cpty_endpoint =
                 cpty_endpoint.ok_or_else(|| anyhow!("Must specify counterparty endpoint"))?;
             let cpty_rpc_client = RpcClient::new(cpty_endpoint);
